5524c6d148fbea4b3b844c09bcdc3daf94ed3453
Steven Hubert <steven.hubert@gmail.com>
2013-10-13 18:14:06 -0600

    initial lesson without refactoring

---
+++ app.rb
@@ -0,0 +1,50 @@
+require './idea'
+
+
+class IdeaBoxApp < Sinatra::Base
+	set :method_override, true
+	
+
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+
+
+	get '/' do
+		erb :index, locals: {ideas: Idea.all}
+	end
+	
+	get '/cats' do
+		"I love cats"
+	end
+	
+	post '/' do
+		idea = Idea.new(params['idea_title'], params['idea_description'])
+		idea.save
+		redirect '/'
+	end
+	
+	not_found do
+		erb :error
+	end
+
+	delete '/:id' do |id|
+		Idea.delete(id.to_i)
+		redirect '/'
+	end
+
+	get '/:id/edit' do |id|
+		idea = Idea.find(id.to_i)
+		erb :edit, locals: {id: id, idea: idea}
+	end
+
+	put '/:id' do |id|
+		data = {
+			:title => params['idea_title'],
+			:description => params['idea_description']
+		}
+		Idea.update(id.to_i, data)
+		redirect '/'
+	end
+end
